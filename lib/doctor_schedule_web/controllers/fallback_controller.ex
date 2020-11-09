03751a5c1b06cc392dac2594e982ba842a62fe85
Gustavo Oliveira <g.92oliveira@gmail.com>
2020-11-09 11:52:17 -0300

    fix fallbackcontroller

--- lib/doctor_schedule_web/controllers/fallback_controller.ex
+++ lib/doctor_schedule_web/controllers/fallback_controller.ex
@@ -6,7 +6,14 @@ defmodule DoctorScheduleWeb.FallbackController do
   """
   use DoctorScheduleWeb, :controller
 
-  # This clause is an example of how to handle resources that cannot be found.
+
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> put_view(DoctorScheduleWeb.ChangesetView)
+    |> render("error.json", changeset: changeset)
+  end
+
   def call(conn, {:error, :not_found}) do
     conn
     |> put_status(:not_found)
